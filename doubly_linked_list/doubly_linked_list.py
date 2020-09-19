ec8808e89eadb1e69a191b5a9293748b49cc50d3
raudelf <rawdell@outlook.com>
2020-09-18 22:53:52 -0500

    :white_check_mark: 90% Complete
    Fixed the ListNode delete function, had to switch the next and prev in the if statements. Also, completed the rest of the functions for the DLL minus the get_max function. That is the only thing left to meet MVP 2

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -11,9 +11,9 @@ class ListNode:
         self.next = next
 
     def delete(self):
-        if self.prev:
-            self.next.prev = self.prev
         if self.next:
+            self.next.prev = self.prev
+        if self.prev:
             self.prev.next = self.next
 
 
@@ -58,7 +58,17 @@ class DoublyLinkedList:
     """
 
     def remove_from_head(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            cur_value = self.head
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+            self.length -= 1
+            return cur_value.value
 
     """
     Wraps the given value in a ListNode and inserts it 
@@ -67,7 +77,18 @@ class DoublyLinkedList:
     """
 
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
 
     """
     Removes the List's current tail node, making the 
@@ -76,8 +97,27 @@ class DoublyLinkedList:
     """
 
     def remove_from_tail(self):
-        pass
+        if self.tail is None and self.head is None:
+            return None
 
+        if self.tail == self.head:
+            ret_value = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return ret_value
+
+        else:
+            ret_value = self.tail.value
+
+            temp_node = self.head
+
+            while temp_node.next is not self.tail:
+                temp_node = temp_node.next
+            temp_node.next(None)
+            self.tail = temp_node
+            self.length -= 1
+            return ret_value
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
@@ -95,7 +135,10 @@ class DoublyLinkedList:
     """
 
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        self.delete(node)
+        self.add_to_tail(node.value)
 
     """
     Deletes the input node from the List, preserving the 
